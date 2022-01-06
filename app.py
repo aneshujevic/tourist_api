73ecbc9d892556d45cbf4a66d20a695e3c05458c
aneshujevic <anes1996_h@hotmail.com>
2022-01-06 19:34:42 +0100

    add command line option create-admin

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import click
+from flask import Flask, current_app
+from flask.cli import with_appcontext
+from werkzeug.security import generate_password_hash
 
 import account_change_request_views
 import arrangement_views
@@ -7,6 +10,7 @@ import reservation_views
 import user_views
 from config import BaseConfig
 from extensions import ma, db, jwt_man, mi, mail
+from models import User
 
 
 def create_app(config_object=BaseConfig()):
@@ -14,6 +18,30 @@ def create_app(config_object=BaseConfig()):
     app.config.from_object(config_object)
     register_extensions(app)
     register_blueprints(app)
+
+    @app.cli.command("create-admin")
+    @click.option("-u", "--username", "username")
+    @click.option("-e", "--email", "email")
+    @click.option("-f", "--first-name", "first_name")
+    @click.option("-l", "--last-name", "last_name")
+    @click.option("-p", "--password", "password")
+    @with_appcontext
+    def create_admin(username, email, first_name, last_name, password):
+        admin = User(
+            username=username,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=generate_password_hash(
+                password,
+                current_app.config.get("PASSWORD_HASH_ALGORITHM"),
+                current_app.config.get("PASSWORD_SALT_LENGTH")
+            )
+        )
+        User.query.session.add(admin)
+        User.query.session.commit()
+        print("Successfully created an admin.")
+
     return app
 
 
